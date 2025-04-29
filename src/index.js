55d9f66d24def7cf549e85c6108b91590ea55a7c
unknown <Hilary V>
2025-04-28 22:04:49 -0400

    My first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// En primer ligar se definen los modulos 
+// O Librerias que necesitaremos en el proyecto
+// en este caso sera solo express
+ const express = require('express');
+
+ // en segundo lugar se definen los objetos
+ // del sistema o propiedades del sistema
+ const app = express();
+ const port = 3000;
+
+ // en tercer lugar se definen las rutas o 
+ // funciones que se van a ejecutar
+app.get('/', (req, res) =>)
