6ebd3d9fc66391903282dec6b8ce71b0668bd2c9
unknown <Hilary V>
2025-05-05 21:55:43 -0400

    Convertir WebPractica de subrama a carpeta normal

---
+++ WebPractica/src/routes/routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const ruta = Router();
+
+//////////////////////////////////////
+
+ruta.get('/', (req, res) => {
+    res.send('ruta raiz');
+});
+
+ruta.get('/alternativa', (req, res) => {
+    res.send('ruta alternativa');
+});
+
+////////////////////////////////////
+
+module.exports = ruta;
